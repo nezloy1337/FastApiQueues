500ae0b16fca47bc36b9e6b0c17ec718f590925d
Andrian Yukhnevich <andy.yukh@gmail.com>
2024-12-29 20:52:37 +0300

    added first and last names to user database

---
+++ fastapi-application/alembic/versions/2024_12_29_2045-7b8614aefb34_added_first_and_last_names_for_user.py
@@ -0,0 +1,31 @@
+"""added first and last names for user
+
+Revision ID: 7b8614aefb34
+Revises: 567f634f61a1
+Create Date: 2024-12-29 20:45:42.156829
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "7b8614aefb34"
+down_revision: Union[str, None] = "567f634f61a1"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("user", sa.Column("first_name", sa.String(), nullable=False))
+    op.add_column("user", sa.Column("last_name", sa.String(), nullable=False))
+
+
+def downgrade() -> None:
+
+    op.drop_column("user", "last_name")
+    op.drop_column("user", "first_name")
+
--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,5 +1,8 @@
 from typing import TYPE_CHECKING
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from sqlalchemy.orm import Mapped, mapped_column
+
+from sqlalchemy import String
 
 from .base import Base
 
@@ -8,6 +11,9 @@ if TYPE_CHECKING:
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
+
+    first_name: Mapped[str] = mapped_column(String,nullable=False)
+    last_name: Mapped[str] = mapped_column(String,nullable=False)
 
     @classmethod
     def get_db(cls,session:"AsyncSession"):
