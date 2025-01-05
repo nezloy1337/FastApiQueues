868cad8c1c98a061e62b1abb2d29cf8ba5a80506
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-05 16:08:28 +0300

    added try except in create_queues_entry

--- fastapi-application/api/api_v1/queues_entries/crud.py
+++ fastapi-application/api/api_v1/queues_entries/crud.py
@@ -4,21 +4,43 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 
 from api.api_v1.queues_entries.schemas import CreateQueueEntryWithAuth
+from core.config import settings
 from core.models import QueueEntries, User
+
+
+import logging
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
 
 
 async def create_queues_entry(
     session: AsyncSession,
     queue_entry_to_create: CreateQueueEntryWithAuth,
     user: User,
-):
-    queue_entry = QueueEntries(
-        **queue_entry_to_create.model_dump(),
-        user_id=str(user.id),
-    )
-    session.add(queue_entry)
-    await session.commit()
-    return queue_entry
+) -> QueueEntries:
+    try:
+        # создание записи в бд
+        queue_entry = QueueEntries(
+            **queue_entry_to_create.model_dump(), user_id=str(user.id)
+        )
+        session.add(queue_entry)
+        await session.commit()
+        return queue_entry
+
+    except IntegrityError as e:
+        # Логирование ошибки
+        logger.error(f"Ошибка при добавлении записи в очередь: {e.args}")
+
+        # Откат транзакции
+        await session.rollback()
+
+        # Возврат HTTPException
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Пользователь уже зарегистрирован в данной очереди.",
+        )
 
 
 async def clear_queues_entry(
