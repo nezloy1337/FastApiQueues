3e88fa981cf86566af0b82feb5fb907a3cc9e996
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-05 21:18:26 +0300

    added exception handeling to queues

--- fastapi-application/api/api_v1/queues/crud.py
+++ fastapi-application/api/api_v1/queues/crud.py
@@ -3,37 +3,55 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from api.api_v1.queues.schemas import CreateQueue
-from api.api_v1.queues_entries.schemas import QueueEntry
 from core.models.queue import Queue,QueueEntries
+from utils.exception_handlers import average_handle_exception
 
 
 async def create_queue(
     session: AsyncSession,
     queue_to_create: CreateQueue,
-):
-    queue = Queue(**queue_to_create.model_dump())
-    session.add(queue)
-    await session.commit()
-    return queue
+) -> Queue:
+    try:
+        #создаем модель и делаем запрос в базу данных
+        queue = Queue(**queue_to_create.model_dump())
+        session.add(queue)
+        await session.commit()
+        return queue
+
+    #обработка ошибки
+    except Exception as e:
+        average_handle_exception(e)
 
 async def get_queues(session: AsyncSession):
-    result = await session.execute(select(Queue))
-    queues = result.scalars().all()
-    return queues
+    try:
+        # создаем запрос и делаем запрос в базу данных
+        query = select(Queue)
+        result = await session.execute(query)
+        queues = result.scalars().all()
+        return queues
+
+    # обработка ошибки
+    except Exception as e:
+        average_handle_exception(e)
 
 async def get_queue_with_entries(session: AsyncSession, queue_id: int):
-
-
-    query = (
-        select(Queue)
-        .outerjoin(Queue.entries)  # Используем outerjoin для левого соединения
-        .outerjoin(QueueEntries.user)  # Используем outerjoin для левого соединения
-        .where(Queue.id == queue_id)
-        .options(
-            selectinload(Queue.entries).selectinload(QueueEntries.user)
+    try:
+        #создание запроса
+        query = (
+            select(Queue)
+            .outerjoin(Queue.entries)  # Используем outerjoin для левого соединения
+            .outerjoin(QueueEntries.user)  # Используем outerjoin для левого соединения
+            .where(Queue.id == queue_id)
+            .options(
+                selectinload(Queue.entries).selectinload(QueueEntries.user)
+            )
         )
-    )
 
-    result = await session.execute(query)
-    queue_with_entries = result.scalars().first()
-    return queue_with_entries
+        #выполнения запроса
+        result = await session.execute(query)
+        queue_with_entries = result.scalars().first()
+        return queue_with_entries
+
+    #обработка ошибок
+    except Exception as e:
+        average_handle_exception(e)
