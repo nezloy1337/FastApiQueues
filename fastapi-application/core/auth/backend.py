97af5f99cc465220ba7865e60860b74d9ad963a5
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-07 17:22:07 +0300

    some style rework

--- fastapi-application/core/auth/backend.py
+++ fastapi-application/core/auth/backend.py
@@ -3,7 +3,6 @@ from fastapi_users.authentication import AuthenticationBackend
 from .transport import bearer_transport
 from .strategy import get_redis_strategy
 
-SECRET = "SECRET"
 
 auth_backend = AuthenticationBackend(
     name="redis",
