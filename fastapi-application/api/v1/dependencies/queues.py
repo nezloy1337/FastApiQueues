d2cebfffb975237526ff8f1068f6a14e1ac9954a
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-12 03:16:07 +0300

    added get_queue_service dependency

---
+++ fastapi-application/api/v1/dependencies/queues.py
@@ -0,0 +1,15 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper
+from repositories.queue import QueueRepository
+from services.queue import QueueService
+
+
+
+
+async def get_queue_service(
+        session: AsyncSession = Depends(db_helper.session_getter)
+) -> QueueService:
+    queue_repository = QueueRepository(session)
+    return QueueService(queue_repository)
