9ed14eb2cc60f62ccaebd03f124d5a40c2904af1
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-13 07:24:04 +0300

    patch tags now with patterns

--- fastapi-application/api/v1/routers/tags.py
+++ fastapi-application/api/v1/routers/tags.py
@@ -1,11 +1,8 @@
 from typing import Annotated, List
 
 from fastapi import APIRouter, status, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.v1.dependencies.tags import get_tags_service
-from api.v1.routers.tags2 import crud
-from core.models import db_helper
 from schemas.tags import CreateTag, GetTag, PatchTag
 from services.tags import TagsService
 
@@ -54,14 +51,14 @@ async def delete_tag(
 
 
 @router.patch(
-    "tags2/{tag_id}",
+    "{tag_id}",
     status_code=status.HTTP_200_OK,
     response_model=PatchTag,
 )
 async def patch_tag(
     tag_id: int,
     tag_patch: PatchTag,
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    service: Annotated[TagsService, Depends(get_tags_service)]
     # user: Annotated[User, Depends(current_user)],
 ):
-    return await crud.patch_tag(tag_id, tag_patch, session)
+    return await service.update(tag_id, tag_patch.model_dump())
