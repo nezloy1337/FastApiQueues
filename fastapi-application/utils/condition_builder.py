eac454fad9d8a8224ff384b1d739ce42faf281c2
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-14 23:44:22 +0300

    condition builder crated

---
+++ fastapi-application/utils/condition_builder.py
@@ -0,0 +1,11 @@
+
+
+class ConditionBuilder:
+    def __init__(self,model):
+        self.model = model
+        self.filters = []
+
+    def create_condition(self, **conditions):
+        for key, value in conditions.items():
+            self.filters.append((getattr(self.model, key) == value))
+        return self.filters
