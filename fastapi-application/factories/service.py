6034cea85074cdc3f0bb268f753596f322d1e2d5
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-16 14:10:05 +0300

    feat: add service factory

---
+++ fastapi-application/factories/service.py
@@ -0,0 +1,23 @@
+from typing import Type, Callable
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper
+from repositories.base import TRepository
+from services.base import TService
+
+
+#нужен ли async
+
+class ServiceFactory:
+    @staticmethod
+    def create(
+        service_cls: Type[TService],
+        repository_cls: Type[TRepository],
+    ) -> Callable[[AsyncSession], TService]:
+        """Создаёт фабрику для сервиса с указанным репозиторием."""
+        def _get_service(session: AsyncSession = Depends(db_helper.session_getter)) -> TService:
+            repository = repository_cls(session)
+            return service_cls(repository)
+        return _get_service
