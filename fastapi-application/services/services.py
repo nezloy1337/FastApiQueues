b31dec968db48e9639d25ea4847f9a1d4431045a
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-16 14:22:46 +0300

    refactor: delete overhead init in services

--- fastapi-application/services/services.py
+++ fastapi-application/services/services.py
@@ -1,33 +1,21 @@
-from repositories.queue import QueueRepository
-from repositories.queue_entry import QueueEntriesRepository
-from repositories.queue_tags import QueueTagsRepository
-from repositories.tags import TagsRepository
-from repositories.user import UserRepository
 from services.base import BaseService
 
 
 class QueueEntryService(BaseService):
-    def __init__(self, repository: QueueEntriesRepository):
-        super().__init__(repository)
+    pass
 
 
 class QueueTagService(BaseService):
-    def __init__(self,repository: QueueTagsRepository):
-        super().__init__(repository)
+    pass
 
 
 class TagsService(BaseService):
-
-    def __init__(self, repository: TagsRepository):
-        super().__init__(repository)
+    pass
 
 
 class UserService(BaseService):
-
-    def __init__(self,repository: UserRepository):
-        super().__init__(repository)
+    pass
 
 
 class QueueService(BaseService):
-    def __init__(self,repository: QueueRepository):
-        super().__init__(repository)
+    pass
