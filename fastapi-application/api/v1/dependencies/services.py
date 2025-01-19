c3678a95b418357fafa0b1414b2f8e05fdd7d14f
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-19 06:32:03 +0300

    refactor:switch from get_service to get_service_by_model dependency

--- fastapi-application/api/v1/dependencies/services.py
+++ fastapi-application/api/v1/dependencies/services.py
@@ -1,42 +1,12 @@
+from factories.service import get_service_by_model
+from models import User, QueueEntries, Tags, Queue, QueueTags
 
-from factories.get_service import get_service
-from repositories import (
-    UserRepository,
-    QueueRepository,
-    QueueTagsRepository,
-    QueueEntriesRepository,
-    TagsRepository,
-)
-from services import (
-    UserService,
-    QueueEntryService,
-    TagsService,
-    QueueService,
-    QueueTagService,
-)
+get_user_service = get_service_by_model(User)
 
+get_queue_entries_service = get_service_by_model(QueueEntries)
 
-get_user_service = get_service(
-    UserService,
-    UserRepository,
-)
+get_tags_service = get_service_by_model(Tags)
 
-get_queue_entries_service = get_service(
-    QueueEntryService,
-    QueueEntriesRepository,
-)
+get_queue_tags_service = get_service_by_model(QueueTags)
 
-get_tags_service = get_service(
-    TagsService,
-    TagsRepository,
-)
-
-get_queue_tags_service = get_service(
-    QueueTagService,
-    QueueTagsRepository,
-)
-
-get_queue_service = get_service(
-    QueueService,
-    QueueRepository,
-)
+get_queue_service = get_service_by_model(Queue)
