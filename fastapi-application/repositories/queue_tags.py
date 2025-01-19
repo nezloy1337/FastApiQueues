7180fcb2cae657bbd3155ddaf957bdac3e4c70e4
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-19 06:31:26 +0300

    refactor:added condition_builder object ot init of the class

--- fastapi-application/repositories/queue_tags.py
+++ fastapi-application/repositories/queue_tags.py
@@ -2,8 +2,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import QueueTags
 from repositories import BaseRepository
+from utils.condition_builder import ConditionBuilder
 
 
 class QueueTagsRepository(BaseRepository):
-    def __init__(self,session: AsyncSession):
-        super().__init__(QueueTags,session,)
+    def __init__(self, session: AsyncSession, condition_builder: ConditionBuilder):
+        super().__init__(
+            QueueTags,
+            session,
+            condition_builder,
+        )
--- fastapi-application/repositories/tags.py
+++ fastapi-application/repositories/tags.py
@@ -2,12 +2,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import Tags
 from repositories import BaseRepository
+from utils.condition_builder import ConditionBuilder
 
 
 class TagsRepository(BaseRepository[Tags]):
 
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: AsyncSession,condition_builder: ConditionBuilder):
         super().__init__(
             Tags,
             session,
+            condition_builder,
         )
