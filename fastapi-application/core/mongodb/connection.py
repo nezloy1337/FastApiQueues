a29df632a39e75707c8ad647be05a31c5d0fe2dd
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-21 05:55:40 +0300

    feat: add connection registry

--- fastapi-application/core/mongodb/connection.py
+++ fastapi-application/core/mongodb/connection.py
@@ -1,3 +1,5 @@
+from typing import Type, Dict
+
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from core.config import settings
@@ -7,4 +9,15 @@ mongodb = client[settings.mongo.db_name]
 
 queue_entries_logs_collection = mongodb["queue_entries"]
 queue_logs_collection = mongodb["queues"]
+users_logs_collection = mongodb["users"]
 error_collection = mongodb['errors']
+
+
+CONNECTION_REGISTRY: Dict[
+    str,
+    Type[mongodb]
+] = {
+    "queues" : queue_logs_collection,
+    "queueEntries": queue_entries_logs_collection,
+    "users": users_logs_collection,
+}
