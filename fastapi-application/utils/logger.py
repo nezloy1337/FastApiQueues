88c49718de53eeefbe1baf36ca5b83e295096afb
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-21 05:49:07 +0300

    feat: add connection_registry

--- fastapi-application/utils/logger.py
+++ fastapi-application/utils/logger.py
@@ -1,20 +1,70 @@
-from core.mongodb.connection import queue_logs_collection, queue_entries_logs_collection
-from core.mongodb.schemas import QueueEntryLog, QueueLog
-from utils.exception_handlers import handle_unexpected_error
+import inspect
+from datetime import datetime
+from functools import wraps
+from typing import Optional, List, Callable
+
+from pydantic import BaseModel
+
+from core.mongodb.connection import CONNECTION_REGISTRY
+from core.mongodb.schemas import ActionLog
 
 
-async def log_queue_entry(**queue_entry_data):
-    try:
-        queue_entry_log = QueueEntryLog(**queue_entry_data)
-        await queue_entries_logs_collection.insert_one(queue_entry_log.model_dump(exclude_none=True))
-    except Exception as e:
-        handle_unexpected_error(e)
+def log_action(
+    action: str, log_params: Optional[List[str]] = None, collection_name: str = "logs"
+) -> Callable:
+    """
+    Декоратор для логирования в MongoDB.
 
-async def log_queue(**queue_data):
-    try:
-        queue_entry_log = QueueLog(**queue_data)
-        await queue_logs_collection.insert_one(queue_entry_log.model_dump(exclude_none=True))
-    except Exception as e:
-        handle_unexpected_error(e)
+    :param action: Описание или тип действия (логически, что мы логируем).
+    :param log_params: Список имён параметров функции, которые нужно залогировать.
+                       Если None, логируются все параметры.
+    :param collection_name: Имя коллекции в MongoDB, куда писать логи.
+    """
+    if log_params is None:
+        log_params = []
 
+    def decorator(func):
 
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            status = "success"
+            try:
+                return await func(*args, **kwargs)
+
+            except Exception as e:
+                status = "failed"
+                raise
+
+            finally:
+                timestamp = datetime.now()
+                sig = inspect.signature(func)
+                bound = sig.bind(*args, **kwargs)
+
+                logged_args = {}
+
+                for name,value in bound.arguments.items():
+                    if name in log_params:
+                        if isinstance(value, BaseModel):
+                            logged_args[name] = value.model_dump()
+                        else:
+                            logged_args[name] = value
+
+                log_entry = ActionLog(
+                    action=action,
+                    parameters=logged_args,  # Только отфильтрованные параметры
+                    status=status,
+                    timestamp=timestamp
+                )
+                # log_entry = {
+                #     "action": action,
+                #     "parameters": logged_args,  # Только отфильтрованные параметры
+                #     "status": status,
+                #     "timestamp": str(timestamp),
+                # }
+
+                logs_collection = CONNECTION_REGISTRY.get(collection_name)
+                await logs_collection.insert_one(log_entry.model_dump())
+
+        return wrapper
+
+    return decorator
