485b393cc26cb76e9b7fe285dd7265ecddb57efb
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-22 14:44:36 +0300

    feat:add delete_all to QueueEntries service and repository

--- fastapi-application/domains/queues/repositories.py
+++ fastapi-application/domains/queues/repositories.py
@@ -1,9 +1,10 @@
-from sqlalchemy import select
+from sqlalchemy import select, delete, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
+from core.base import TModels
 from core.base.repository import BaseRepository
-from domains.queues import Queue, QueueEntries
+from domains.queues import Queue, QueueEntries, QueueTags
 from utils.condition_builder import ConditionBuilder
 
 """
@@ -46,6 +47,25 @@ class QueueRepository(BaseRepository[Queue]):
 class QueueEntriesRepository(BaseRepository[QueueEntries]):
     def __init__(self,session: AsyncSession, condition_builder: ConditionBuilder):
         super().__init__(QueueEntries,session,condition_builder)
+
+    async def delete_all(self, filters: dict) -> TModels | None:
+        """
+        Удаляет объекты, соответствующие указанным условиям, возвращает все
+        удалённые объекты.
+        Если объект не найдены, возвращается None.
+
+        :param filters: Произвольные условия для фильтрации объектов.
+        :return: Удалённый объект или None, если ничего не удалено.
+        """
+        query_conditions = self.condition_builder.create_conditions(**filters)
+
+        stmt = delete(self.model).filter(and_(*query_conditions)).returning(self.model)
+
+        result = await self.session.execute(stmt)
+        deleted_obj = result.scalars().first()
+        if deleted_obj:
+            await self.session.commit()
+        return deleted_obj
 
 
 class QueueTagsRepository(BaseRepository):
--- fastapi-application/domains/queues/services.py
+++ fastapi-application/domains/queues/services.py
@@ -1,8 +1,38 @@
+from typing import Any
+
+from fastapi import HTTPException
+from starlette import status
+
 from core.base.services import BaseService
+from domains.queues import QueueRepository
+from utils.exception_handlers import handle_exception
 
 
 class QueueEntryService(BaseService):
-    pass
+    def __init__(
+        self,
+        repository: QueueRepository,
+    ):
+        super().__init__(repository)
+
+    @handle_exception
+    async def delete_all(self, filters: dict[str, Any]) -> bool:
+        """
+        Удаляет объекты, соответствующий условиям.
+
+        :param filters: Фильтры поиска объектов
+        (например, {"queue_id": 1,"user_id":2}).
+        :return: True, если объект успешно удалён.
+        :raises HTTPException: Если объект не найден.
+        """
+        deleted_obj = await self.repository.delete_all(filters)
+        if deleted_obj:
+            return True
+
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Not Found",
+        )
 
 
 class QueueTagService(BaseService):
