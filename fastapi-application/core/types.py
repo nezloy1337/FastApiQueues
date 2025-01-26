e72312aa980b9f708e2f3efe93730af5d19bb18b
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-26 17:30:27 +0300

    fix: fix TypeVars

--- fastapi-application/core/types.py
+++ fastapi-application/core/types.py
@@ -1,23 +1,28 @@
 from typing import TypeVar, Union, TYPE_CHECKING
 
 
-
 if TYPE_CHECKING:
-    pass
+    from core.base import Base, BaseService# noqa: F401
+    from domains.queues import QueueTagsRepository, QueueEntriesRepository# noqa: F401
+    from domains.tags import TagsRepository# noqa: F401
+    from domains.users import UserRepository # noqa: F401
+
 
 TRepositories = TypeVar(
     "TRepositories",
-    bound=Union["QueueRepository "],
+    bound=Union[
+        "QueueRepository",
+        "TagsRepository",
+        "QueueTagsRepository",
+        "QueueEntriesRepository",
+        "UserRepository",
+    ],
 )
 
 
-TService = TypeVar(
-    "TService",
-    bound="BaseService"
-)
+TService = TypeVar("TService", bound="BaseService")
 
 TModels = TypeVar(
     "TModels",
     bound="Base",
 )
-
