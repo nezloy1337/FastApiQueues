29ac5a847620fa5f1de964dc7fa3ef06e27296f8
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-01-26 11:48:30 +0300

    refactor: move TTypes to separate module

--- fastapi-application/core/types.py
+++ fastapi-application/core/types.py
@@ -1,6 +1,19 @@
+from typing import TypeVar, Union, TYPE_CHECKING
 
 
 
+if TYPE_CHECKING:
+    pass
 
+TRepositories = TypeVar(
+    "TRepositories",
+    bound=Union["QueueRepository "],
+)
+
+
+TService = TypeVar(
+    "TService",
+    bound="BaseService"
+)
 
 
