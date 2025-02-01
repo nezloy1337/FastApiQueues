12d38f35b38e4f4b7ee91b4bd658b3c342672053
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-02-01 11:11:45 +0300

    style(pyproject.toml,locustfile)

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,7 @@ from locust import HttpUser, TaskSet, task, between
 class UserBehavior(TaskSet):
     def on_start(self):
         # Токен авторизации
-        self.token = "Bearer iACoW07K75c9Pru09oMgGxTc97vp1WsBqwmJQht8WVM"
+        self.token = "Bearer copy_one_here"
         self.headers = {
             "Authorization": self.token,
             "Content-Type": "application/json"
@@ -22,4 +22,4 @@ class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
     wait_time = between(1, 3)
 
-#     locust -f load_testing.py
+#locust -f load_testing.py
