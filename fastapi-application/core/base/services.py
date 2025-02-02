d133a6ac0fd4c3f5d59a3b8d4f4d54e4c47420df
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-02-02 21:03:39 +0300

    fix: fix HTTPException code

--- fastapi-application/core/base/services.py
+++ fastapi-application/core/base/services.py
@@ -66,7 +66,7 @@ class BaseService(Generic[TModels, TRepositories]):
             return True
 
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Not Found",
         )
 
