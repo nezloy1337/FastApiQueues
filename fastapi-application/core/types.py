9af0612415bb5bdfdf27b332ad244fa43affad95
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-02-08 01:38:42 +0300

    feat: add JSONSerializable (v1)

--- fastapi-application/core/types.py
+++ fastapi-application/core/types.py
@@ -1,4 +1,7 @@
-from typing import TYPE_CHECKING, Any, TypeVar  # noqa: F401
+from datetime import datetime
+from typing import TYPE_CHECKING, Any, TypeAlias, TypeVar, Union  # noqa: F401
+
+from bson import ObjectId
 
 if TYPE_CHECKING:
     from core.base import Base, BaseRepository, BaseService  # noqa: F401
@@ -14,6 +17,11 @@ if TYPE_CHECKING:
     from domains.tags import TagsRepository, TagsService  # noqa: F401
     from domains.users import UserRepository, UserService  # noqa: F401
 
+
+JSONSerializable: TypeAlias = Union[
+    str, int, float, bool, list, dict, None, datetime, ObjectId
+]
+MongoDocument = dict[str, JSONSerializable]
 TRepositories = TypeVar(  # ошибка unbound
     "TRepositories",
     bound="BaseRepository[Any]",
