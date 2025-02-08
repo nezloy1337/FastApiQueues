3c4a3ffbbe7c660ef26fed6a61f8eb1afb108458
Andrian Yukhnevich <andy.yukh@gmail.com>
2025-02-08 17:07:42 +0300

    docs(connection): add docs

--- fastapi-application/core/mongodb/connection.py
+++ fastapi-application/core/mongodb/connection.py
@@ -6,21 +6,27 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 
 from core.config import settings
 
+# Define collection names and serializable types
 CollectionName = Literal["queues", "queue_entries", "users", "errors", "failed_to_get"]
-
-# Определяем тип документа MongoDB
 JSONSerializable = Union[str, int, float, bool, list, dict, None, datetime, ObjectId]
 
 
 class MongoConnectionManager:
+    """
+    Manages MongoDB connection and provides access to predefined collections.
+    """
+
     def __init__(self, url: str, db_name: str) -> None:
         self.client = AsyncIOMotorClient(
             url,
             uuidRepresentation="standard",
         )
+
         self.db = self.client[db_name]
+
         self._registry: dict[
-            CollectionName, AsyncIOMotorCollection[Mapping[str, JSONSerializable]]
+            CollectionName,
+            AsyncIOMotorCollection[Mapping[str, JSONSerializable]],
         ] = {
             "queues": self.db["queues"],
             "queue_entries": self.db["queue_entries"],
@@ -32,8 +38,14 @@ class MongoConnectionManager:
     def get_collection(
         self, name: CollectionName
     ) -> AsyncIOMotorCollection[Mapping[str, JSONSerializable]]:
+        """
+        Retrieves an AsyncIOMotorCollection for the given collection name.
+        """
         return self._registry[name]
 
 
 def get_mongo_manager() -> MongoConnectionManager:
+    """
+    Creates and returns a MongoConnectionManager instance.
+    """
     return MongoConnectionManager(settings.mongo.url, settings.mongo.db_name)
